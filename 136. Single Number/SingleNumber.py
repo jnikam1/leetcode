91d75b3421971a7a152fd863f18b7d6893544974
Jayesh Nikam <jnikam1@binghamton.edu>
2022-02-10 18:30:53 -0500

    SingleNumber

---
+++ 136. Single Number/SingleNumber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict1= Counter(nums)
+        # print (dict1)
+        for i,v in dict1.items():
+            if (v==1):
+                return i
